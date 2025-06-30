43025248c2de4a69dd031ef5c19d6ba2356b61c8
Cesar Sustaita <c.sustaita@outlook.com>
2025-06-29 19:40:34 -0600

    Change: the configuration has been changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,27 +4,26 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'UASLP UI Kit',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss:[
-				//Ruta alternativa al css personalizado
-				'./src/styles/uaslp-styles.css',
-			]
-		}),
+    integrations: [starlight({
+        title: 'UASLP UI Kit',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        sidebar: [
+            {
+                label: 'Getting Started',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Introducción', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Styles Guide',
+                autogenerate: { directory: 'StylesGuide' },
+            },
+        ],
+        customCss:[
+            //Ruta alternativa al css personalizado
+            './src/styles/uaslp-styles.css',
+        ]
+		}), 
 	],
 });
